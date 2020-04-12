36a674e98f1866c3c8096107b0f6c8abe5c46b99
mans-18 <miguel.sza@gmail.com>
2020-04-12 02:54:46 -0300

    Added create ingredients feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -24,10 +24,10 @@ class TagViewSet(viewsets.GenericViewSet,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
-        """Overrride teh perform_create function and create a new tag with an
+        """Overrride the perform_create function and create a new tag with an
         auth user"""
-        # When create is invoked, perform_create is called, received serialiser
-        # as arg, so one can customize teh creation: set the user to the auth
+        # When create is invoked, perform_create is called, receives serializer
+        # as arg, so one can customize the creation: set the user to the auth
         # user.
         serializer.save(user=self.request.user)
 
@@ -36,7 +36,8 @@ class TagViewSet(viewsets.GenericViewSet,
 # the error occurs: 'django.urls.exceptions.NoReverseMatch: Reverse for
 # 'ingredient-list' not found. 'ingredient-list' is not a valid view function
 # or pattern name'
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
@@ -45,3 +46,7 @@ class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current auth user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        # Atention to the arg of save(). 'user' is in the request
+        serializer.save(user=self.request.user)
