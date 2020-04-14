d913669bf15b99e5389e3acb7840f2ccaabb7bb0
mans-18 <miguel.sza@gmail.com>
2020-04-13 21:12:58 -0300

    Added patch and put tests

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -194,3 +194,49 @@ class PrivateRecipeApiTests(TestCase):
         # null value in column "user_id" violates not-null constraint
         # DETAIL:  Failing row contains (4, Avocado lime cheesecake, 60, 20.00,
         # ,null).
+
+    def test_partial_update_recipe(self):
+        """Test updating a recipe with patch"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        new_tag = sample_tag(user=self.user, name='Curry')
+
+        payload = {'title': 'Chicken tikka', 'tags': [new_tag.id]}
+        url = detail_url(recipe.id)
+        self.client.patch(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 1)
+        self.assertIn(new_tag, tags)
+
+    def test_full_update_recipe(self):
+        """Test updating a recipe with put"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+        payload = {
+            'title': 'Spaghetti carbonara',
+            'time_minutes': 25,
+            'price': 5.00,
+            # AssertionError: Test data contained a dictionary value for
+            # key 'ingredients', but multipart uploads do not support nested
+            # data. You may want to consider using format='json' in this
+            # test case.
+            #            'ingredients': {'name': 'macorron',
+            #                            'name': 'tomato sauce',
+            #                            'name': 'meat'
+            #                            }
+        }
+        url = detail_url(recipe.id)
+        self.client.put(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+        self.assertEqual(recipe.price, payload['price'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 0)
+        ingredients = recipe.ingredients.all()
+        self.assertEqual(len(ingredients), 0)
