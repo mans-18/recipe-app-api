b483a0e8fd133ddba57b479970bb331cc5cdbda5
mans-18 <miguel.sza@gmail.com>
2020-04-15 15:08:04 -0300

    Added recipe image upload feature

--- app/core/models.py
+++ app/core/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 
 def recipe_image_file_path(instance, filename):
-    """Generate file pathfor new recipe image"""
+    """Generate file path for new recipe image"""
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
 
